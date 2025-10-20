3529b270eacee96f39025b6fe9151b148bf7d91d
AaishaShahani2001 <shahani6360@gmail.com>
2025-10-20 15:17:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: allowedOrigins, credentials: true }));
-app.use("/uploads", express.static(path.join(__dirname, "uploads", "medical"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads", "medical")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads", "slips")));
 
 //--------------------------- Route --------------------------//
