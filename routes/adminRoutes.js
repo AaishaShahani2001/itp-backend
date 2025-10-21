d5a314d69f6d63b4f5f75fcdaa2d835cf42fc2ea
Dilush14 <diludilush963@gmail.com>
2025-10-21 17:26:34 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addCareTaker, addDoctor, cancelAdoption, changeAdoptionStatus, getAdoptions, getAllUsers, getDashboardData, getPets, loginAdmin, getAllReviews, deleteReview } from '../controllers/adminController.js';
+import { addCareTaker, updateCareTaker, deleteCareTaker, addDoctor, updateDoctor, deleteDoctor, cancelAdoption, changeAdoptionStatus, getAdoptions, getAllUsers, getDashboardData, getPets, loginAdmin, getAllReviews, deleteReview, listDoctors, listCaretakers } from '../controllers/adminController.js';
 import authAdmin from '../middleware/authAdmin.js';
 import upload from '../middleware/multer.js';
 
@@ -11,7 +11,13 @@ adminRouter.post("/login", loginAdmin)
 adminRouter.put("/change-status", authAdmin, changeAdoptionStatus)
 adminRouter.get("/dashboard", authAdmin, getDashboardData)
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor)
+adminRouter.put("/update-doctor/:id", authAdmin, upload.single("image"), updateDoctor);
+adminRouter.delete("/delete-doctor/:id", authAdmin, deleteDoctor);
+adminRouter.get("/doctors", authAdmin, listDoctors)
 adminRouter.post("/add-caretaker", authAdmin, upload.single("image"), addCareTaker)
+adminRouter.put("/update-caretaker/:id", authAdmin, upload.single("image"), updateCareTaker);
+adminRouter.delete("/delete-caretaker/:id", authAdmin, deleteCareTaker);
+adminRouter.get("/caretakers", authAdmin, listCaretakers)
 adminRouter.delete("/cancel-adoption/:adoptionId", authAdmin, cancelAdoption)
 adminRouter.get("/get-user", authAdmin, getAllUsers)
 
